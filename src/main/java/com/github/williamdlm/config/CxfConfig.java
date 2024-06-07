f03dc62003b1b0d1dec0e665c73f0e972118bc27
William Mota <william.mota.ext@spms.min-saude.pt>
2024-06-07 13:48:56 +0100

    Added annotation beaninject to CxfConfig

--- src/main/java/com/github/williamdlm/config/CxfConfig.java
+++ src/main/java/com/github/williamdlm/config/CxfConfig.java
@@ -1,9 +1,11 @@
 package com.github.williamdlm.config;
 
 import com.github.williamdlm.service.GreetingService;
+import org.apache.camel.BeanInject;
 import org.apache.camel.component.cxf.jaxws.CxfEndpoint;
 
 public class CxfConfig {
+    @BeanInject
     public CxfEndpoint greetingEndpoint() {
         CxfEndpoint cxfEndpoint = new CxfEndpoint();
         cxfEndpoint.setAddress("http://localhost:8080/greeting");
